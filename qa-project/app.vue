6369e53e3a486a358cb4ee70b25c02ac1adda9ef
fasika <fasikafikadu38@gmail.com>
2024-10-08 23:04:31 +0300

    feat: Implement landing page UI and initial project setup

---
+++ qa-project/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center bg-black w-full h-full p-3">
+    <index />
+  </div>
+</template>
+
+<script>
+import index from "./pages/login/index.vue";
+export default {
+  components: { index },
+  css: ["@/assets/css/tailwind.css"],
+  // other configurations...
+};
+</script>
